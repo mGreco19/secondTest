4e06995cc92ec67915e8a4d3c7368bd0d19a26e7
mGreco19 <moiso1908@gmail.com>
2018-06-20 20:27:21 -0600

    commit01

--- src/java/entities/forecast.java
+++ src/java/entities/forecast.java
@@ -5,7 +5,91 @@ package entities;
  * @author Moises
  */
 public class forecast {
-    //hey whatsup?
-    //www
-    //hello again
+    int mes;
+    int dia;
+    String periodo; 
+    int estado;
+    String descripcion;
+    int minima;
+    int maxima;
+    int llovera;
+    
+    public forecast(){}
+    
+    public forecast( int mes, int dia, String periodo, 
+    int estado, String descripcion, int minima, int maxima, int llovera){
+        this.mes=mes;
+        this.dia=dia;
+        this.periodo=periodo;
+        this.estado=estado;
+        this.descripcion=descripcion;
+        this.minima=minima;
+        this.maxima=maxima;
+        this.llovera=llovera;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getMinima() {
+        return minima;
+    }
+
+    public void setMinima(int minima) {
+        this.minima = minima;
+    }
+
+    public int getMaxima() {
+        return maxima;
+    }
+
+    public void setMaxima(int maxima) {
+        this.maxima = maxima;
+    }
+
+    public int getLlovera() {
+        return llovera;
+    }
+
+    public void setLlovera(int llovera) {
+        this.llovera = llovera;
+    }
+    
 }
