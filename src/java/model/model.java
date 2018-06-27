bada22989bf8d7339fabc80cd4a510a13608a8d7
mGreco19 <moiso1908@gmail.com>
2018-06-26 22:55:24 -0600

    commit05

--- src/java/model/model.java
+++ src/java/model/model.java
@@ -1,9 +1,34 @@
 package model;
 
+import dao.dao;
+import entities.forecast;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author Moises
  */
 public class model {
+    
+    private dao Dao;
+    
+    private static model uniqueInstance;
+    
+    public static model instance(){
+        
+     if (uniqueInstance == null){
+            uniqueInstance = new model();
+        }
+        return uniqueInstance;
+    }
+    
+    private model(){
+        Dao=new dao();  
+    }
+    
+    public List<forecast> getForecast(int d,int m) throws SQLException{
+        return Dao.getForecast(d, m);
+    }
     
 }
